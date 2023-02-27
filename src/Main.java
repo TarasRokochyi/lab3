e2e9d562559c61160794f941242ffa65e52bfe2d
TarasRokochyi <tarrok2005@gmail.com>
2023-02-27 14:35:11 +0200

    1 task

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,91 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scan = new Scanner(System.in);
+        while (true){
+            System.out.println("Task:");
+            String text = scan.nextLine();
+            if(text.equals("1")){
+                while (true) {
+                    System.out.println("Go or q");
+                    text = scan.nextLine();
+                    if (text.equals("go")) {
+                        int a = Integer.parseInt(scan.nextLine());
+                        for(int i = 10;i < a; i++){
+                            if ((i % 2) == 1){
+                                System.out.println(i * i);
+                            }
+                        }
+                    }
+                    else if (text.equals("q")) {
+                        break;
+                    }
+                }
+            }
+            else if(text.equals("2")) {
+                while (true) {
+                    System.out.println("Go or q");
+                    text = scan.nextLine();
+                    if (text.equals("go")) {
+
+                    }
+                    else if (text.equals("q")) {
+                        break;
+                    }
+                }
+            }
+            else if(text.equals("3")){
+                while (true) {
+                    System.out.println("Go or q");
+                    text = scan.nextLine();
+                    if (text.equals("go")) {
+
+                    }
+                    else if (text.equals("q")) {
+                        break;
+                    }
+                }
+            }
+            else if(text.equals("4")) {
+                while(true) {
+                    System.out.println("Go or q");
+                    text = scan.nextLine();
+                    if (text.equals("go")) {
+
+                    }
+                    else if (text.equals("q")) {
+                        break;
+                    }
+                }
+            }
+            else if(text.equals("5")){
+                while(true) {
+                    System.out.println("Go or q");
+                    text = scan.nextLine();
+                    if (text.equals("go")) {
+
+                    }
+                    else if (text.equals("q")) {
+                        break;
+                    }
+                }
+            }
+            else if(text.equals("6")){
+                while (true) {
+                    System.out.println("Go or q");
+                    text = scan.nextLine();
+                    if (text.equals("go")) {
+
+                    }
+                    else if (text.equals("q")) {
+                        break;
+                    }
+                }
+            }
+            else if(text.equals("q")){
+                break;
+            }
+        }
     }
 }
